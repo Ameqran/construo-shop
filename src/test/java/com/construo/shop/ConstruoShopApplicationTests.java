29ae3016a60dc36d4e71227dc4cea919bf7d1e32
Ameqran <marouaneboukedir@gmail.com>
2025-12-30 18:25:11 +0100

    CI-1 Bootstrap Spring Boot app

---
+++ src/test/java/com/construo/shop/ConstruoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construo.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
